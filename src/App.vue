6338c2ae652537abfaba8aaff73f07574b1b39d2
feenA <zxyz1v1m1@163.com>
2021-04-25 01:25:46 +0800

    push

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <!-- <app-head></app-head> -->
+  <firstScreen></firstScreen>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import firstScreen from './components/firstScreen.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    firstScreen
   }
 })
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ src/components/firstScreen.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+  class='firstScreen w-screen h-screen bg-local opacity-60 bg-cover relative'
+  style='background-image: url("/bg.jpg")'>
+    <div></div>
+    <h5 class='font-serif text-current absolute bottom-1 right-2 opacity-100'>illustrate: 12345</h5>
+  </div>
+</template>
+
+<script lang='ts'>
+export default {
+  name: 'firstScreen',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+
+</style>
